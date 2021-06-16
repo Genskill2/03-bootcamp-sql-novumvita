c88b47dd12c045495bf6194b489c8e8f1e7c7714
darshan <j.darshan2008@gmail.com>
2021-06-16 20:53:25 +0530

    TILsqlitedoesntsupportinnerjoindelete

--- delete1.sql
+++ delete1.sql
@@ -1,8 +1,6 @@
-DElETE FROM books_subjects
-FROM books_subjects
-INNER JOIN subjects
-ON books_subjects.subject = subjects.id 
-WHERE subjects.name IS "History";
+DELETE FROM books_subjects
+WHERE subject
+IS (SELECT id FROM subjects WHERE name IS "History");
 
 DELETE FROM subjects 
 WHERE subjects.name IS "History";
